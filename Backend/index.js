8b1042a756a2a2f0ce317f2a597ffa8be3ccb68c
Ebru18 <ebrubendeliyeva@gmail.com>
2025-01-09 11:57:24 +0400

    first nodejs project

---
+++ Backend/index.js
@@ -0,0 +1,63 @@
+const express = require("express")
+const app = express() 
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const dotenv = require("dotenv")
+const cors = require("cors");
+app.use(bodyParser.json())
+app.use(cors())
+dotenv.config()
+app.get("/", (req, res) => {
+    res.send("welcome")
+})
+let ProductShema = new mongoose.Schema({
+    name: String,
+    description: String,
+    price: Number
+})
+let ProductModel = mongoose.model("products", ProductShema)
+app.get("/products", async (req, res) => {
+    let products = await ProductModel.find()
+    res.send(products)
+})
+
+app.get("/products/:id", async (req, res) => {
+    let { id } = req.params
+    let product = await ProductModel.findById(id)
+    res.send({
+        message: "Succes getById",
+        data: product
+    })
+})
+
+
+app.delete("/products/:id", async (req, res) => {
+    let { id } = req.params
+    await ProductModel.findByIdAndDelete(id)
+    res.send({
+        message: "Succes Delete",
+
+    })
+})
+
+
+
+
+app.post("/products", async (req, res) => {
+    let newProduct = ProductModel(req.body)
+    await newProduct.save()
+    res.send({
+        message: "Success Post",
+        data: req.body
+    })
+})
+ 
+
+
+mongoose.connect(process.env.ConnectionString)
+    .then(() => {
+        console.log("connected");
+    })
+app.listen(3000, () => {
+    console.log("bu app 3000 portunda dinlenilir") 
+})
